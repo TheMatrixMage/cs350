cad636bd75e9f9990076b58a877a49a96d3a2485
emstaffieri <emstaffieri@mix.wvu.edu>
2017-04-19 14:19:31 -0400

    Add files via upload

---
+++ queue.c
@@ -0,0 +1,19 @@
+#include "queue.h"
+#include "job.h"
+#include <stdlib.h>
+
+Queue * CreateQueue() {
+
+}
+void DestroyQueue(Queue * queue) {
+
+}
+void Enqueue(Queue * queue, Job * job) {
+
+}
+Job * Dequeue(Queue * queue) {
+
+}
+int GetQueueSize(Queue * queue) {
+
+}
---
+++ queue.h
@@ -0,0 +1,18 @@
+
+#ifndef GUARD
+#define GUARD
+
+typedef struct Queue {
+	Job * first;
+	int size;
+}Queue;
+
+Queue * CreateQueue();
+void DestroyQueue(Queue * queue);
+void Enqueue(Queue * queue, Job * job);
+Job * Dequeue(Queue * queue);
+int GetQueueSize(Queue * queue);
+
+
+
+#endif
